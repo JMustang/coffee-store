e6403b4c4305fb3b6d82c065225577c55eda1cb7
Junior <eecfredes@gmail.com>
2024-11-09 19:26:02 -0300

    :rocket: server structure

--- coffee-server/cmd/server/main.go
+++ coffee-server/cmd/server/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	Port string `json:"port"`
+}
+
+type Application struct {
+	config Config
+}
+
+func (app *Application) Server() error {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error leading .env file")
+	}
+	port := os.Getenv("PORT")
+	fmt.Println("API is running on port:", port)
+
+	srv := &http.Server{
+		Addr: fmt.Sprintf(":%s", port),
+		// TODO: add router Handler: app.routes(),
+	}
+	return srv.ListenAndServe()
+}
 
 func main() {
-	fmt.Println("API is running on port:")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error leading .env file")
+	}
 }
